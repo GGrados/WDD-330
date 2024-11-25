c734ea41556e832602dd0ab8b3acc76ec092aafa
Roque Alberto Pulido Morales <rapulidom@desarrollo-ultrasist.com.mx>
2024-11-25 16:40:38 -0600

    ADD template header & footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,5 @@
-import Alert from "./Alert";
-import ProductData from "./ProductData.mjs";
+import { loadHeaderAndFooter } from "./utils.mjs";
 
-import ProductList from "./ProductList.mjs";
-
-const dataSource = new ProductData("tents");
-const element = document.querySelector(".product-list");
-const listing = new ProductList("Tents", dataSource, element);
-
-listing.init();
-
-import ProductListing from "./ProductList.mjs";
-import { getMenucartLogo } from "./utils.mjs";
-
-const listElement = document.querySelector(".product-list");
-
-const producData = new ProductData("tents");
-
-const productList = new ProductListing("tents", producData, listElement);
-productList.init();
-
-// Get the logo cart with the number of elements in cart
-getMenucartLogo(".cart");
-
-const listAlert = new Alert;
-listAlert.getAlerts();
+// Get header and footer
+loadHeaderAndFooter();
 
