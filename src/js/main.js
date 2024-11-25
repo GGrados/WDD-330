e99684225839e033713a03810ad6d75f65825534
Roque Alberto Pulido Morales <rapulidom@desarrollo-ultrasist.com.mx>
2024-11-25 17:37:30 -0600

    ADD alert

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 import { loadHeaderAndFooter } from "./utils.mjs";
+import Alert from "./Alert.js";
 
 // Get header and footer
 loadHeaderAndFooter();
 
+const listAlert = new Alert();
+listAlert.getAlerts();
